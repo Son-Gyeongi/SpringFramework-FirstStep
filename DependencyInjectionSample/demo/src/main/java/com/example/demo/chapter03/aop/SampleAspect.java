9995a6f248056a1917a52b616881dd3fa0453899
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-06 10:24:41 +0900

    chapter03: aop @Around

--- DependencyInjectionSample/demo/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
+++ DependencyInjectionSample/demo/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
@@ -1,7 +1,9 @@
 package com.example.demo.chapter03.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -29,9 +31,10 @@ public class SampleAspect {
 
     /**
      * 메서드 실행 후 호출하는 After Advice
+     *
      * @param joinPoint
      */
-    @After("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+    /*@After("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
     public void afterAdvice(JoinPoint joinPoint) {
         // 시작 부분 표시
         System.out.println("===== After Advice =====");
@@ -39,5 +42,16 @@ public class SampleAspect {
         System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new Date()));
         // 메서드 이름 출력
         System.out.println(String.format("메서드:%s", joinPoint.getSignature().getName()));
+    }*/
+    @Around("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+    public Object aroundAdvice(ProceedingJoinPoint joinPoint) throws Throwable {
+        // 시작 부분 표시
+        System.out.println("===== Around Advice =====");
+        System.out.println("▼▼▼ 처리전 ▼▼▼");
+        // 지정한 클래스의 메서드 실행
+        Object result = joinPoint.proceed();
+        System.out.println("▲▲▲ 처리후 ▲▲▲");
+        // 반환값을 돌려줄 필요가 있는 경우에는 Object 타입의 반환값을 돌려줍니다.
+        return result;
     }
 }
