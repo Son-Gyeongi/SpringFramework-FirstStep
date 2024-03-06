58b32432e9196e6349a24247ecc04c5841a00229
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-06 10:17:46 +0900

    chapter03: aop @After

--- DependencyInjectionSample/demo/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
+++ DependencyInjectionSample/demo/src/main/java/com/example/demo/chapter03/aop/SampleAspect.java
@@ -1,6 +1,7 @@
 package com.example.demo.chapter03.aop;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,12 +14,27 @@ import java.util.Date;
 public class SampleAspect {
     /**
      * 메서드 실행 전에 호출하는 Before Advice
+     *
      * @param joinPoint
      */
-    @Before("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+    /*@Before("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
     public void beforeAdvice(JoinPoint joinPoint) {
         // 시작 부분 표시
         System.out.println("===== Before Advice =====");
+        // 날짜 출력
+        System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new Date()));
+        // 메서드 이름 출력
+        System.out.println(String.format("메서드:%s", joinPoint.getSignature().getName()));
+    }*/
+
+    /**
+     * 메서드 실행 후 호출하는 After Advice
+     * @param joinPoint
+     */
+    @After("execution(* com.example.demo.chapter03.used.*Greet.*(..))")
+    public void afterAdvice(JoinPoint joinPoint) {
+        // 시작 부분 표시
+        System.out.println("===== After Advice =====");
         // 날짜 출력
         System.out.println(new SimpleDateFormat("yyyy/MM/dd").format(new Date()));
         // 메서드 이름 출력
