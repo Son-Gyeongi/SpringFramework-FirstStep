ae848491bc4e6b561593f6a41eb982152e5f4c7c
Son-Gyeongi <thsruddl77@gmail.com>
2024-03-09 20:12:20 +0900

    chapter11: make application - 비즈니스 로직 처리

--- quiz/quiz/src/main/java/com/example/quiz/repository/QuizRepository.java
+++ quiz/quiz/src/main/java/com/example/quiz/repository/QuizRepository.java
@@ -1,10 +1,14 @@
 package com.example.quiz.repository;
 
 import com.example.quiz.entity.Quiz;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Quiz 테이블: RepositoryImpl
  */
 public interface QuizRepository extends CrudRepository<Quiz, Integer> {
+    // quiz 테이블의 id 칼럼을 랜덤으로 1건 가져오는 SQL
+    @Query("SELECT id FROM quiz ORDER BY RANDOM() limit 1")
+    Integer getRandomId();
 }
